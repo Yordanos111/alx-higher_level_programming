302dbae90ff2a6342b43133fddcb529b9a77b466
Yordanos111 <jor.esh55@gmail.com>
2022-06-03 02:30:25 +0300

    My commit for task 2, correction for ':'

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,6 +8,6 @@ if __name__ == "__main__":
         print('{:d} argument:'.format((len(sys.argv))-1))
         print('{:d}:'.format((len(sys.argv))-1), sys.argv[len(sys.argv)-1])
     else:
-        print('{:d} arguments'.format((len(sys.argv))-1))
+        print('{:d} arguments:'.format((len(sys.argv))-1))
         for i in range(1, len(sys.argv)):
             print('{:d}:'.format(i), sys.argv[i])
