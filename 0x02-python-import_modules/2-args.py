a0fa8e2aa72eab5ab3a75de64e6c93f2b4521f18
Yordanos111 <jor.esh55@gmail.com>
2022-06-03 02:14:36 +0300

    My commit for task 2.

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,10 +2,10 @@
 if __name__ == "__main__":
     import sys
     if len(sys.argv) == 1:
-        print('0 arguments')
+        print('0 arguments.')
         exit()
     if len(sys.argv) == 2:
-        print('{:d} argument'.format((len(sys.argv))-1))
+        print('{:d} argument:'.format((len(sys.argv))-1))
         print('{:d}:'.format((len(sys.argv))-1), sys.argv[len(sys.argv)-1])
     else:
         print('{:d} arguments'.format((len(sys.argv))-1))
