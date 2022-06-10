ebffa0839be8432f160a523a215456d290022bc1
Yordanos111 <jor.esh55@gmail.com>
2022-06-10 04:39:41 +0300

    My commit for task 7, changed

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    if value in a_dictionary:
-        for i in a_dictionary:
-            if i == key:
-                a_dictionary[i] = value
-    else:
-        a_dictionary[key] = value
-    return a_dictionary
+    a_dictionary[key] = value
+    return (a_dictionary)
