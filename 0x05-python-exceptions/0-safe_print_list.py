50fe390d0dfff5f201b06f0dc78156b67d4f43d3
Yordanos111 <jor.esh55@gmail.com>
2022-06-22 22:27:36 +0300

    My commit for Task 0.

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,7 +4,7 @@ def safe_print_list(my_list=[], x=0):
         return 0
     count = 0
     try:
-        for i in range(y):
+        for i in range(x):
             print("{}".format(my_list[i]), end="")
             count = count + 1
         print()
