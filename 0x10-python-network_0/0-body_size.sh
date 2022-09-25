2e21180ff4a20251ab593a2c8b612a4639157d8c
Yordanos111 <jor.esh55@gmail.com>
2022-09-25 23:02:24 +0300

    0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This program takes in a URL, sends a request to that URL, and displays the size of the body of the response.
-curl -sI "$1" | grep "Content-Length:" | cut -c 17-
+curl -sI "$1" | grep 'Content-Length' | awk '{print $2}'
